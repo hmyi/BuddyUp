4ca37a61c4ed1df222ca0cebf8c1fde11b7f13c3
hmyi <hmtimyi@gmail.com>
2025-02-03 20:54:14 +0000

    SCRUM-29: Link EC2 Backend w/ RDS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,12 +111,15 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "buddyup",
+        "USER": "django_user",
+        "PASSWORD": "strong_password",
+        "HOST": "buddyupdb.cnmm82wkmdjb.us-east-2.rds.amazonaws.com",
+        "PORT": "5432",
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
