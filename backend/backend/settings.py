0c8f23ee33841ed71d26f6190ef1618b96c42c96
hmyi <hmtimyi@gmail.com>
2025-02-05 22:41:12 +0000

    debug invald characters

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -113,14 +113,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "buddyup",
-        "USER": "django_user",
-        "PASSWORD": "strong_password",
-        "HOST": "buddyupdb.cnmm82wkmdjb.us-east-2.rds.amazonaws.com",
-        "PORT": "5432",
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "buddyup",
+        "USER": "django_user",
+        "PASSWORD": "strong_password",
+        "HOST": "buddyupdb.cnmm82wkmdjb.us-east-2.rds.amazonaws.com",
+        "PORT": "5432",
+    }
 }
 
 # Password validation
