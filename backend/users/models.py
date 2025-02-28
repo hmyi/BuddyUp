bf9d67b6246815cc4e3b30c2663bcb0295c140ea
Shuzheng Zheng <shuzheng9811@gmail.com>
2025-02-27 21:38:04 -0500

    Resolve SQLite and PostGreSQL diff in testing

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class User(AbstractUser):
     # Store user facebook id
     facebook_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
 
+    def clean(self):
+        """Ensure Facebook ID does not exceed max_length."""
+        if self.facebook_id and len(self.facebook_id) > 100:
+            raise ValidationError("Facebook ID cannot exceed 100 characters.")
+    
     def __str__(self):
         return self.username
