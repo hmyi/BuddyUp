e48fcf4c0c948d61cd5f73342d511aeaeff6bd3e
Shuzheng Zheng <shuzheng9811@gmail.com>
2025-03-08 00:54:53 -0500

    Fix django migration conflict

--- backend/events/migrations/0004_event_cancelled.py
+++ backend/events/migrations/0004_event_cancelled.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0002_event_vector'),
+        ('events', '0003_event_vector'),
     ]
 
     operations = [
